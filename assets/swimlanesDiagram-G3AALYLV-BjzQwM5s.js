9df7bd8ae2ca5e86f6f641fc5316e0070a67b389
Kenji Fujimoto <kenji.fujimoto@change-vision.com>
2026-09-11 19:07:18 +0900

    Deploy SPA to GitHub Pages (v0.5.16)

---
+++ assets/swimlanesDiagram-G3AALYLV-BjzQwM5s.js
@@ -0,0 +1,8 @@
+import"./src-BMa7vLb8.js";import"./chunk-WYO6CB5R-D6HFq0NG.js";import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./chunk-ICXQ74PX-D0fs9-NE.js";import"./chunk-C7G6YPKG-CGCGRvAW.js";import"./chunk-OGEWGWER-UYT19sxw.js";import"./chunk-HOUHSVGY-Bx5fTz1U.js";import"./chunk-Q4XR5HBZ-CicCg7n7.js";import"./chunk-ZGVPDNZ5-oqCpSHf4.js";import"./chunk-7BUUIJ7U-Bb538aSH.js";import"./index-DFZ6jxim.js";import"./chunk-32BRIVSS-UbmSlk7m.js";import"./chunk-XXDRQBXY-D6m_P2Gv.js";import"./chunk-VR4S4FIN-BwWR265C.js";import{r as t,t as n}from"./chunk-PUDLZKDR-Z1VxnxtI.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
